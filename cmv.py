1585f5cb365e772f234d7ddd223e61e228672973
Bhupendra Raut <bhupendra.raut@northwestern.edu>
2021-10-06 11:22:21 -0500

    working, CMV plot looks OK, noQC

--- cmv.py
+++ cmv.py
@@ -8,19 +8,21 @@ Created on Mon Oct 4 10:03:50 2021
 
 import argparse
 import cv2 as cv
-#from matplotlib import pyplot as plt
-from phase_correlation import fft_flowvectors
+import numpy as np
+from matplotlib import pyplot as plt
 from sys import exit
-from time import sleep
+
+from cmvUtils import flowVectorSplit, fftFlowVector
 
 parser = argparse.ArgumentParser(description='''This program uses phase correlation method from the TINT module
                                               to compute the cloud motion vectors in the hemispheric camera video''')
 parser.add_argument('--input', type=str, help='Path to an input video or images.', default="./data/sgptsimovieS01.a1.20160726.000000.mpg")
-parser.add_argument('--fskip', type=str, help='Background subtraction method (KNN, MOG2).', default=30)
+parser.add_argument('--fskip', type=str, help='Background subtraction method (KNN, MOG2).', default=2)
 
 args = parser.parse_args()
 
-
+chan = 2 # The program does not use all the RGB channels
+nblock = 7
 
 print('Opening:', args.input)
 
@@ -39,7 +41,8 @@ print("Original Frame Height : '{}'".format(video_cap.get(cv.CAP_PROP_FRAME_HEIG
 
 fcount = 0
 first_frame = True
-
+plt.ion()
+flow_plot= plt.figure()
 while video_cap.isOpened():
     ret, frame = video_cap.read()
     fcount +=1 
@@ -51,7 +54,7 @@ while video_cap.isOpened():
     
     if fcount==1 or fcount % args.fskip == 0:
         print('Current Frame:', fcount)
-        sky = frame[101:549, 16:464, :]
+        sky = frame[101:549, 16:464, chan] #too specific
         
         #Store the sky data for first the frame as .
         if first_frame:
@@ -64,11 +67,37 @@ while video_cap.isOpened():
         sky_curr = sky
         
         
-        mean_motion = fft_flowvectors(sky_prev[:, :, 2], sky_curr[:, :, 2], global_shift=True)
+        
+        mean_motion = fftFlowVector(sky_prev, sky_curr, global_shift=True)
         print(mean_motion)
         
-        cv.imshow("Original", frame)
-        cv.waitKey(1000)
+        cmv_x, cmv_y = flowVectorSplit(sky_prev, sky_curr, nblock)
+        
+
+        #comput centra point for plotting arrows
+        arrow_loc = np.arange(31, 448, 64)
+        
+        plt.imshow(sky_prev)
+        plt.quiver(arrow_loc, arrow_loc, cmv_y, -cmv_x)
+        plt.pause(0.3)
+        plt.close()
+
+
+plt.show()
+plt.ioff()
+
+        
+        
+        
+        #cv.imshow("Original", frame)
+        #cv.waitKey(1000)
+        #exit()
+        
+        
+        
+        
+        
+
         
 
     
